0bef5b0193a44f903145f8ea4f17fd7cdfd7502f
ziffyhilya <ziffy.hilya@students.untidar.ac.id>
2026-08-30 15:59:15 +0700

    Add files via upload

---
+++ Ziffy_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ziffy_project1;
+
+/**
+ *
+ * @author Pongo
+ */
+public class Ziffy_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ziffy Hilya");
+        System.out.println("NPM : 2505060008");
+        System.out.println("Alamat : Salaman, Magelang");
+        System.out.println("No.HP : 08567281728");
+    }
+}
