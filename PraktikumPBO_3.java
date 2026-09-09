4941bba6919e503b7f26c95a5f66fcfdba4bee72
ziffyhilya <ziffy.hilya@students.untidar.ac.id>
2026-09-09 01:10:08 +0000

    Add files via upload

---
+++ PraktikumPBO_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikumpbo_3;
+
+/**
+ *
+ * @author Pongo
+ */
+public class PraktikumPBO_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
